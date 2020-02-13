af3685a67b85ca083b98efd863bdf33261adb728
flaskur <flaskur@gmail.com>
2020-02-13 13:15:44 -0500

    inverse matrix, matrix mult, affine constant thought

--- s_box_forward.v
+++ s_box_forward.v
@@ -9,6 +9,10 @@ module s_box_forward (
   output [7:0] output_data
 );
 
+// Abstract view, s-box takes the rounded key which is input XOR key as the actual input_data. The s-box operation takes the multiplicative inverse of the input_data. This I have yet to figure out a method for doing. With this multiplicative inverse, I need to do matrix multiplication with the affine matrix which I can represent row-wise using 8 registers. Then I need to add the affine constant which is 0x63. That's the entire answer. Don't worry about performance right now.
+
+reg [7:0] affine_row1 = {}
+
 
 endmodule;
 
