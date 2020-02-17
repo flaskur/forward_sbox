bdf9eb1a029207c72315f796ef942ba43e56e9f1
flaskur <flaskur@gmail.com>
2020-02-16 19:04:15 -0500

    bit mod edit

--- bitmod.v
+++ bitmod.v
@@ -2,10 +2,10 @@
 
 // This is the actual calculation portion. I need to give the normal 8 bit input and return a single bit based on the formula with modulus. I also need the affine constant.
 module bitmod # (
-  parameter AFFINE_CONSTANT = 8'b0110_0011 // 0x63
+  parameter [7:0] AFFINE_CONSTANT = 8'h63 
 )(
   input clk,
-  input ibit, // Meaning which particular bit index you want like b0, b1, etc.
+  input [2:0] ibit, // Meaning which particular bit index you want like b0, b1, etc.
   input [7:0] idata,
   output obit
 );
@@ -14,8 +14,15 @@ reg output_bit;
 
 assign obit = output_bit;
 
+// Following the bit modulus formula given on wikipedia, this finds the single bit value for a particular index, ibit.
 always @(posedge clk) begin
-  output_bit <= idata[ibit] ^ idata[(ibit + 4) % 8] ^ idata[(ibit + 5) % 8] ^ idata[(ibit + 6) % 8] ^ idata[(ibit + 7) % 8] ^ AFFINE_CONSTANT[ibit];
+    output_bit <= idata[ibit] ^ idata[(ibit + 4) % 8] ^ idata[(ibit + 5) % 8] ^ idata[(ibit + 6) % 8] ^ idata[(ibit + 7) % 8] + AFFINE_CONSTANT[ibit];
+end
+
+// Debugging.
+always @(*) begin
+    $display("%3b", ibit);
+    $display("%b", obit);
 end
 
 endmodule
